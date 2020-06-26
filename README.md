42c364c174a3fc32720e310c71f848cc087d3d9f
Li Tan <litan@login5.summit.olcf.ornl.gov>
2020-06-26 15:28:03 -0400

    Initial push

--- README.md
+++ README.md
@@ -23,13 +23,15 @@ export RMQ_HOSTNAME=129.114.17.233
 export RADICAL_PILOT_DBURL="mongodb://rct:rct_test@129.114.17.233:27017/rct_test"
 ```
 
+Note that installation of the binaries used by either CPU tasks or GPU tasks are not needed, since we are using pre-installed binaries on Summit in shared locations from other efforts.
+
 ## Resource Configuration
 
 - `res_desc` includes:
    - 'resource': HPC facility name, e.g., 'ornl.summit'
    - 'queue':
-      - 'batch' for upto 2 hours TTX at `ornl.summit`
-      - 'killable' for upto 1 day(24hours) TTX at `ornl.summit`
+      - 'batch' for up to 2 hours time to completion at `ornl.summit`
+      - 'killable' for up to 24 hours time to completion at `ornl.summit`
    - 'cpus': On Summit each node has 42 cores and each core has 4 hardware threads, so we consider 168 parallel threads
    - 'gpus': a number of GPU cores equal to N simulations (1 MD/ML run: 1 GPU)
 
---
+++ benchmark_gpu.py
@@ -0,0 +1,140 @@
+from radical import entk
+import os
+import argparse, sys
+
+class BENCHMARK(object):
+
+    def __init__(self):
+        self._set_rmq()
+        self.am = entk.AppManager(hostname=self.rmq_hostname, port=self.rmq_port)
+        self.p = entk.Pipeline()
+        self.s = entk.Stage()
+
+    def _set_rmq(self):
+        self.rmq_port = int(os.environ.get('RMQ_PORT', 33239))
+        self.rmq_hostname = os.environ.get('RMQ_HOSTNAME', 'two.radical-project.org')
+
+    def set_resource(self, res_desc):
+        res_desc["schema"] = "local"
+        self.am.resource_desc = res_desc
+
+    def sim(self, task_count):
+
+        #os.system("export RADICAL_PROFILE=\"TRUE\"")
+        #os.system("export RADICAL_PILOT_PROFILE=\"TRUE\"")
+        #os.system("export RADICAL_ENTK_PROFILE=\"TRUE\"")
+        #os.system("export RADICAL_VERBOSE=\"DEBUG\"")
+        #os.system("export RADICAL_LOG_LVL=\"DEBUG\"")
+        #os.system("export RADICAL_LOG_TGT=\"radical.log\"")
+
+        gromacs_path = '/gpfs/alpine/world-shared/csc393/hrlee/gromacs-5.1.5/build/bin/gmx'
+        gromacs_example_path = '/gpfs/alpine/world-shared/csc393/hrlee/'
+
+        for i in range(1, task_count + 1):
+
+            t = entk.Task()
+
+            """if i <= 20:
+                t.pre_exec = [
+                    "module load gcc/7.4.0",
+                    "export OUTDIR=\"/gpfs/alpine/med110/scratch/litan/FFEA/output/rep{}\"".format(i),
+                    "mkdir -p $OUTDIR"
+                ]
+                t.executable = '/gpfs/alpine/csc299/scratch/litan/FFEA/ffea-2.6.0/build/bin/ffea'
+                t.arguments = ['/gpfs/alpine/csc299/scratch/litan/FFEA/ffea-2.6.0/build/bin/input/emd_5043_albert_E1GPa.ffea']
+                t.post_exec = []
+                t.cpu_reqs = {
+                    'processes': 1,
+                    'process_type': None,
+                    'threads_per_process': 1,
+                    'thread_type': 'OpenMP'
+                }
+                t.gpu_reqs = {
+                    'processes': 0,
+                    'process_type': None,
+                    'threads_per_process': 1,
+                    'thread_type': 'CUDA'
+                }"""
+            '''if i > 6:
+                t.executable = '/bin/date'''
+            if i > 6:
+                t.pre_exec = []
+                t.executable = '/gpfs/alpine/csc299/scratch/litan/INCITE_benchmark/hello_world'
+                t.arguments = []
+                t.post_exec = []
+
+                t.cpu_reqs = {
+                    'processes': 100,
+                    'process_type': 'MPI',
+                    'threads_per_process': 1,
+                    'thread_type': 'OpenMP'
+                }
+                '''t.gpu_reqs = {
+                    'processes': 0,
+                    'process_type': None,
+                    'threads_per_process': 1,
+                    'thread_type': 'CUDA'
+                }'''
+            else:
+                '''t.pre_exec = [
+                    ##"export OMP_NUM_THREADS=1",
+                    "export INPATH=\"$MEMBERWORK/med110/inpath\"",
+                    ". /ccs/home/litan/miniconda3/etc/profile.d/conda.sh",
+                    "conda activate wf3",
+                    "module load cuda/10.1.243 gcc/7.4.0 spectrum-mpi/10.3.1.2-20200121",
+                    "mkdir -p $INPATH; cd $INPATH",
+                    "export OUTPATH=$INPATH",#"export OUTPATH=\"$INPATH/rep{}\"".format(i),
+                    "mkdir -p $OUTPATH; cd $OUTPATH",
+                    "rm -f traj.dcd sim.log"
+                ]
+                t.executable = '/ccs/home/litan/miniconda3/envs/wf3/bin/python3.7'
+                t.arguments = ['$INPATH/sim_esmacs.py', '-i$INPATH', '-n0.2', '-r{}'.format(i)]
+                t.post_exec = []'''
+                grompp_bin = (gromacs_path + ' grompp -f ' + 
+                    (' %s/gromacs_example/inp_files/grompp.mdp' % gromacs_example_path) +
+                     ' -c ' +
+                    (' %s/gromacs_example/inp_files/input.gro' % gromacs_example_path) +
+                     ' -p ' +
+                    (' %s/gromacs_example/inp_files/topol.top' % gromacs_example_path) +
+                     ' &> grompp.log')
+                t.pre_exec = ["module load gcc/8.1.1", grompp_bin]
+                t.executable = gromacs_path
+                t.arguments = ['mdrun']
+                t.post_exec = []
+                t.cpu_reqs = {
+                    'processes': 1,
+                    'process_type': None,
+                    'threads_per_process': 4,
+                    'thread_type': 'OpenMP'
+                }
+                t.gpu_reqs = {
+                    'processes': 1,
+                    'process_type': None,
+                    'threads_per_process': 1,
+                    'thread_type': 'CUDA'
+                }
+
+            self.s.add_tasks(t)
+
+        self.p.add_stages(self.s)
+
+    def run(self):
+        self.am.workflow = [self.p]
+        self.am.run()
+
+
+if __name__ == "__main__":
+
+    benchmark = BENCHMARK()
+
+    n_nodes = 1
+    benchmark.set_resource(res_desc = {
+        'resource': 'ornl.summit',
+        'queue'   : 'batch',
+        'walltime': 1440, #MIN
+        'cpus'    : 168 * n_nodes,
+        'gpus'    : 6 * n_nodes,
+        'project' : 'MED110'
+        })
+    benchmark.sim(task_count=6)
+    benchmark.run()
